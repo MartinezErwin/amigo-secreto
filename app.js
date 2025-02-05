f5a4c21eacbd97db1392bd526125dfe03a2728cb
MartinezErwin <612110504@utsalamanca.edu.mx>
2025-02-05 03:01:51 -0600

    Agregar función para añadir amigos y validar entrada

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
 let amigos = [];
+
+
+function agregarAmigo() {
+    //Capturando el valor del campo de entrada:
+    let nombre = document.getElementById('amigo').value;
+    
+    //Validando la entrada
+    nombre === '' ? alert('No dejes el campo vacio'):
+
+    //Actualizar el array de amigos
+    amigos.push(nombre);
+
+    //Limpiando el campo de entrada:
+    document.getElementById('amigo').value = '';
+
+    //Imprimiendo en la consola el resultado
+    console.log(amigos);   
+}
+  
+
